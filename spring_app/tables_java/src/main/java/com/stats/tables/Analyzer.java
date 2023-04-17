9ebfece53f9902eae835e88f753be7ade0b0a997
Hohenöcker, Julian <Julian.Hohenoecker@adesso.de>
2023-04-17 14:23:05 +0200

    Remove commented code

--- spring_app/tables_java/src/main/java/com/stats/tables/Analyzer.java
+++ spring_app/tables_java/src/main/java/com/stats/tables/Analyzer.java
@@ -1,12 +1,14 @@
 package com.stats.tables;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +19,9 @@ public class Analyzer implements CommandLineRunner {
     private DataSource dataSource;
 
     @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Hello from run(...)");
+    public void run(String... args) {
         List<TableColumnRowCounts> tableStatistics =  this.getTableStatistics();
         printTableStatistics(tableStatistics);
-
     }
 
     public static void printTableStatistics(List<TableColumnRowCounts> tables) {
@@ -32,18 +32,7 @@ public class Analyzer implements CommandLineRunner {
     }
 
     public List<TableColumnRowCounts> getTableStatistics() {
-        // driverType "oci" employs IPC (inter process communication) and is recommended,
-        // when JDBC-Client and oracle-Server runs on the same machine.
-//        String driverType = "thin";
-//        String host = "localhost";
-//        String port = "1521";
-//        String sid = "freepdb1";
-//        String username = "hr";
-//        String pw = "oracle";
-//
-//        String url = String.format("jdbc:oracle:%s:%s/%s@%s:%s/%s", driverType, username, pw, host, port, sid);
 
-//        try (Connection connection = DriverManager.getConnection(url)) {
         try (Connection connection = dataSource.getConnection()) {
             Statement statement = connection.createStatement();
 
@@ -59,16 +48,6 @@ public class Analyzer implements CommandLineRunner {
                        AND t.TABLE_NAME = c.TABLE_NAME
                        AND t.TABLESPACE_NAME = 'USERS'
                      """;
-//            String selectSql = "SELECT t.TABLE_NAME, c.NUM_COLS, r.NUM_ROWS "
-//                    + "FROM"
-//                    + "  ALL_TABLES t, "
-//                    + "  ALL_TAB_STATISTICS r, "
-//                    + "  (SELECT s.TABLE_NAME, COUNT(*) NUM_COLS"
-//                    + "  FROM ALL_TAB_COL_STATISTICS s"
-//                    + "  GROUP BY s.TABLE_NAME) c "
-//                    + "WHERE t.TABLE_NAME = r.TABLE_NAME"
-//                    + "  AND t.TABLE_NAME = c.TABLE_NAME"
-//                    + "  AND t.TABLESPACE_NAME = 'USERS'";
 
             ResultSet queryResult = statement.executeQuery(selectSql);
             System.out.println("Executed query");
@@ -87,8 +66,6 @@ public class Analyzer implements CommandLineRunner {
         } catch (SQLException e) {
             System.out.println("Got an exception");
             throw new RuntimeException(e);
-        } finally {
-            System.out.println("In finally block");
         }
     }
 }
