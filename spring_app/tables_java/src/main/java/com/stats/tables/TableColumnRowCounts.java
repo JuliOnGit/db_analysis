6f6a3960706edf8e19ee6cee10853ccbb4279b6e
Hohenöcker, Julian <Julian.Hohenoecker@adesso.de>
2023-04-17 14:15:49 +0200

    Add java spring boot commandline application with jdbc access

---
+++ spring_app/tables_java/src/main/java/com/stats/tables/TableColumnRowCounts.java
@@ -0,0 +1,34 @@
+package com.stats.tables;
+
+public class TableColumnRowCounts {
+    private String tableName;
+    private int columnCount;
+    private int rowCount;
+
+    public TableColumnRowCounts() {
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public int getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(int columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+}
---
+++ spring_app/tables_java/src/test/java/com/stats/tables/TablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stats.tables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
